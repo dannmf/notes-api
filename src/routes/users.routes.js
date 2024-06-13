7fa830dac3e6691c7f070a1c9d56616e6457df1e
daniel.manoel <daniellfmanoel@gmail.com>
2024-06-13 19:55:36 -0300

    feat: controller e update para imagem ser salva no banco

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,14 +1,21 @@
 
 const { Router } = require("express");
 const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const multer = require("multer");
+const uploadConfig = require("../config/upload");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
  
 usersRoutes.post("/", usersController.createUser);
 usersRoutes.put("/", ensureAuthenticated ,usersController.update);
 usersRoutes.get("/", ensureAuthenticated, usersController.index);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
+
 
 module.exports = usersRoutes;
